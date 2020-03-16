71874888847cbe4d24002369800b4240c9f04a32
Oscar Kramer <oscarkramer@users.noreply.github.com>
2020-03-16 09:37:32 -0400

    Removed PS code

--- zenortho.sh
+++ zenortho.sh
@@ -4,14 +4,12 @@ inputImage=$1
 outputImage=$2
 
 ORTHOISM_HOME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-#ORTHOISM_EXE=/home/okramer/dev/orthoism/build/bin/foo
 ORTHOISM_EXE="orthoism"
-ORTHOISM_ICON=$ORTHOISM_HOME/pslogo.png
 
 function launchZenity()
 {
   zenity --progress \
-  --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+  --title="ORTHOISM"  \
   --text="Orthorectifying '$inputImage'" \
   --percentage=0 \
   --width=1000
@@ -22,12 +20,12 @@ function launchZenity()
     kill -9 $OPID
     pgrep $ORTHOISM_EXE
     zenity --error \
-      --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+      --title="ORTHOISM" \
       --text="Process canceled." \
       --width=500
   elif [[ "$RESULT" != 0 ]]; then
     zenity --error \
-      --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+      --title="ORTHOISM" \
       --text="Run-time error encountered." \
       --width=500
   fi
@@ -35,21 +33,21 @@ function launchZenity()
 
 if [ -z "$inputImage" ]; then
   zenity --error \
-    --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+    --title="ORTHOISM" \
     --text="No file names specified. Please provide input and output file names." \
     --width=500
   exit 1
 fi
 if [ ! -f "$inputImage" ]; then
   zenity --error \
-    --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+    --title="ORTHOISM" \
     --text="Input file '$inputImage' not found. Check the path and try again." \
     --width=500
   exit 1
 fi
 if [ -z "$outputImage" ]; then
   zenity --error \
-    --title="ORTHOISM" --window-icon=$ORTHOISM_ICON \
+    --title="ORTHOISM" \
     --text="An output file name must be specified." \
     --width=500
   exit 1
